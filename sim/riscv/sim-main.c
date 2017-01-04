b4341078a08a6dfd239d57ccb997c1d51fda3a99
Kuan-Lin Chen <rufus@andestech.com>
2017-01-04 15:19:56 +0800

    Support SYS_gettimeofday in gdb-sim.

--- sim/riscv/sim-main.c
+++ sim/riscv/sim-main.c
@@ -26,6 +26,7 @@
 #include <inttypes.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 #include "sim-main.h"
 #include "sim-fpu.h"
@@ -1457,6 +1458,20 @@ execute_i (SIM_CPU *cpu, unsigned_word iw, const struct riscv_opcode *op)
 		  cpu->a0 = cpu->endbrk;
 		else
 		  cpu->endbrk = cpu->a0;
+		break;
+	      }
+	    case TARGET_SYS_gettimeofday:
+	      {
+		int rv;
+		struct timeval tv;
+
+		rv = gettimeofday (&tv, 0);
+		sim_core_write_unaligned_4 (cpu, cpu->pc, write_map,
+					    cpu->a0, tv.tv_sec);
+		sim_core_write_unaligned_4 (cpu, cpu->pc, write_map,
+					    cpu->a0 + RISCV_XLEN (cpu),
+					    tv.tv_usec);
+		cpu->a0 = rv;
 		break;
 	      }
 	    default:
