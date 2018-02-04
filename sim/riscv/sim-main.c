b8771b3edb67410abc3ccfc39d9291cdec8462d6
Kito Cheng <kito.cheng@gmail.com>
2018-02-05 07:10:36 +0800

    (sim) Issue Illegal instruction signal if instruction not decode successful.

--- sim/riscv/sim-main.c
+++ sim/riscv/sim-main.c
@@ -2758,6 +2758,7 @@ riscv_decode (SIM_CPU *cpu, unsigned_word iw, sim_cia pc, int ex9)
   SIM_DESC sd = CPU_STATE (cpu);
   const struct riscv_opcode *op;
   int xlen = RISCV_XLEN (cpu);
+  int is_executed = 0;
 
   op = riscv_hash[OP_HASH_IDX (iw)];
   if (!op)
@@ -2776,7 +2777,12 @@ riscv_decode (SIM_CPU *cpu, unsigned_word iw, sim_cia pc, int ex9)
 	continue;
       /* It's a match.  */
       pc = execute_one (cpu, iw, op, ex9);
+
+      is_executed = 1;
     }
+
+  if (!is_executed)
+    sim_engine_halt (sd, cpu, NULL, pc, sim_signalled, SIM_SIGILL);
 
   return pc;
 }
