d9cb58fa3b6e4d29e23fafdbbbadb4c97f9d5d26
Monk Chiang <monk@andestech.com>
2019-01-09 10:52:35 +0800

    (sim) Add pkbb32, pkbt32, pktb32, pktt32.

--- sim/riscv/sim-main.c
+++ sim/riscv/sim-main.c
@@ -3653,6 +3653,18 @@ execute_p (SIM_CPU *cpu, unsigned_word iw, const struct riscv_opcode *op, int ex
 	TRACE_REG (cpu, rd);
       }
       break;
+    case MATCH_PKTT32:
+      {
+#if (WITH_TARGET_WORD_BITSIZE == 64)
+	cpu->regs[rd].b32.i0 = cpu->regs[rb].b32.i1;
+	cpu->regs[rd].b32.i1 = cpu->regs[ra].b32.i1;
+	TRACE_REG (cpu, rd);
+#else
+	TRACE_INSN (cpu, "UNHANDLED INSN: %s", op->name);
+	sim_engine_halt (sd, cpu, NULL, cpu->pc, sim_signalled, SIM_SIGILL);
+#endif
+      }
+      break;
     case MATCH_PKTB16:
       {
 	/* Rt[31:0] = CONCAT(Ra[31:16], Rb[15:0]) */
@@ -3666,6 +3678,18 @@ execute_p (SIM_CPU *cpu, unsigned_word iw, const struct riscv_opcode *op, int ex
 				| cpu->regs[rb].ub16.h2;
 #endif
 	TRACE_REG (cpu, rd);
+      }
+      break;
+    case MATCH_PKTB32:
+      {
+#if (WITH_TARGET_WORD_BITSIZE == 64)
+	cpu->regs[rd].b32.i0 = cpu->regs[rb].b32.i0;
+	cpu->regs[rd].b32.i1 = cpu->regs[ra].b32.i1;
+	TRACE_REG (cpu, rd);
+#else
+	TRACE_INSN (cpu, "UNHANDLED INSN: %s", op->name);
+	sim_engine_halt (sd, cpu, NULL, cpu->pc, sim_signalled, SIM_SIGILL);
+#endif
       }
       break;
      case MATCH_PKBT16:
@@ -3683,6 +3707,18 @@ execute_p (SIM_CPU *cpu, unsigned_word iw, const struct riscv_opcode *op, int ex
 	TRACE_REG (cpu, rd);
       }
       break;
+    case MATCH_PKBT32:
+      {
+#if (WITH_TARGET_WORD_BITSIZE == 64)
+	cpu->regs[rd].b32.i0 = cpu->regs[rb].b32.i1;
+	cpu->regs[rd].b32.i1 = cpu->regs[ra].b32.i0;
+	TRACE_REG (cpu, rd);
+#else
+	TRACE_INSN (cpu, "UNHANDLED INSN: %s", op->name);
+	sim_engine_halt (sd, cpu, NULL, cpu->pc, sim_signalled, SIM_SIGILL);
+#endif
+      }
+      break;
     case MATCH_PKBB16:
       {
 	/* Rt[31:0] = CONCAT(Ra[15:0], Rb[15:0]) */
@@ -3696,6 +3732,18 @@ execute_p (SIM_CPU *cpu, unsigned_word iw, const struct riscv_opcode *op, int ex
 				| cpu->regs[rb].ub16.h2;
 #endif
 	TRACE_REG (cpu, rd);
+      }
+      break;
+    case MATCH_PKBB32:
+      {
+#if (WITH_TARGET_WORD_BITSIZE == 64)
+	cpu->regs[rd].b32.i0 = cpu->regs[rb].b32.i0;
+	cpu->regs[rd].b32.i1 = cpu->regs[ra].b32.i0;
+	TRACE_REG (cpu, rd);
+#else
+	TRACE_INSN (cpu, "UNHANDLED INSN: %s", op->name);
+	sim_engine_halt (sd, cpu, NULL, cpu->pc, sim_signalled, SIM_SIGILL);
+#endif
       }
       break;
     case MATCH_SMMUL:
