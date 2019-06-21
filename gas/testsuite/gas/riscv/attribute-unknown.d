d581fef0542e8839e8b0cf615fd6917f19a34835
Rex Zhuo <rex.zhuo@outlook.com>
2019-06-21 15:55:37 +0800

    (fix) testsuite hacks for Andes V5 extension

--- gas/testsuite/gas/riscv/attribute-unknown.d
+++ gas/testsuite/gas/riscv/attribute-unknown.d
@@ -3,6 +3,8 @@
 #source: attribute-unknown.s
 Attribute Section: riscv
 File Attributes
-  Tag_RISCV_arch: [a-zA-Z0-9_\"].*
+  Tag_RISCV_arch: [a-zA-Z0-9_\"\-].*
+  Tag_RISCV_priv_spec: 1
+  Tag_RISCV_priv_spec_minor: 10
   Tag_unknown_255: "test"
   Tag_unknown_256: 123 \(0x7b\)
