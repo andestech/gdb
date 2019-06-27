173cba82b812b1d4bc10c3a112317cad2c9d6d8e
Kuan-Lin Chen <rufus@andestech.com>
2019-06-27 16:53:51 +0800

    Don't define __global_pointer$ when CREATE_SHLIB.

--- ld/emulparams/elf32lriscv-defs.sh
+++ ld/emulparams/elf32lriscv-defs.sh
@@ -34,7 +34,7 @@ DATA_START_SYMBOLS="${CREATE_SHLIB-__DATA_BEGIN__ = .;}"
 
 SDATA_START_SYMBOLS=". = ALIGN ($ELFSIZE / 8);
     ${CREATE_SHLIB-__SDATA_BEGIN__ = .;}
-    __global_pointer$ = __SDATA_BEGIN__ + 0x800;
+    ${CREATE_SHLIB-__global_pointer$ = __SDATA_BEGIN__ + 0x800;}
     *(.srodata.cst16) *(.srodata.cst8) *(.srodata.cst4) *(.srodata.cst2) *(.srodata .srodata.*)"
 
 INITIAL_READONLY_SECTIONS=".interp         : { *(.interp) } ${CREATE_PIE-${INITIAL_READONLY_SECTIONS}}"
