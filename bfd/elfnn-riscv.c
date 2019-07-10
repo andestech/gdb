a8687769eb68986696c38fda4f89c2488fc5c75f
Rex Zhuo <rex.zhuo@outlook.com>
2019-07-10 16:41:21 +0800

    (fix) check and init '__global_pointer$' only when linking non-pic

--- bfd/elfnn-riscv.c
+++ bfd/elfnn-riscv.c
@@ -5789,7 +5789,7 @@ _bfd_riscv_relax_section (bfd *abfd, asection *sec,
   riscv_init_pcgp_relocs (&pcgp_relocs);
 
   /* Check and init '__global_pointer$'.  */
-  if (!gp_init)
+  if (!gp_init && !bfd_link_pic (info))
     {
       if (!riscv_init_global_pointer (sec->output_section->owner, info))
 	{
