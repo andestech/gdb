107c1d183f969d473606b69f3797f42a5be36ec2
Rex Zhuo <rex.zhuo@outlook.com>
2019-08-22 14:25:01 +0800

    RISC-V: add Andes Zfh extension

--- gas/config/tc-riscv.c
+++ gas/config/tc-riscv.c
@@ -1896,6 +1896,11 @@ macro (struct riscv_cl_insn *ip, expressionS *imm_expr,
 		  BFD_RELOC_RISCV_PCREL_HI20, BFD_RELOC_RISCV_PCREL_LO12_I);
       break;
 
+    case M_FLH:
+      pcrel_load (rd, rs1, imm_expr, "flh",
+		  BFD_RELOC_RISCV_PCREL_HI20, BFD_RELOC_RISCV_PCREL_LO12_I);
+      break;
+
     case M_FLW:
       pcrel_load (rd, rs1, imm_expr, "flw",
 		  BFD_RELOC_RISCV_PCREL_HI20, BFD_RELOC_RISCV_PCREL_LO12_I);
@@ -1923,6 +1928,11 @@ macro (struct riscv_cl_insn *ip, expressionS *imm_expr,
 
     case M_SD:
       pcrel_store (rs2, rs1, imm_expr, "sd",
+		   BFD_RELOC_RISCV_PCREL_HI20, BFD_RELOC_RISCV_PCREL_LO12_S);
+      break;
+
+    case M_FSH:
+      pcrel_store (rs2, rs1, imm_expr, "fsh",
 		   BFD_RELOC_RISCV_PCREL_HI20, BFD_RELOC_RISCV_PCREL_LO12_S);
       break;
 
