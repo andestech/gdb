dariocastillo11/binutils-gdb-objdump-plugin

Fix column alignment in "maint info line-table"
Andrew Burgess pointed out on irc that "maint info line-table" doesn't
properly align the table headers.  This patch fixes the problem by
switching the table to use ui-out.

This required a small tweak to one test case, as ui-out will pad a
field using spaces, even at the end of a line.

gdb/ChangeLog
2020-03-20  Tom Tromey  <tromey@adacore.com>

	* symmisc.c (maintenance_print_one_line_table): Use ui_out.

gdb/testsuite/ChangeLog
2020-03-20  Tom Tromey  <tromey@adacore.com>

	* gdb.dwarf2/dw2-ranges-base.exp: Update regular expressions.

gdb/symmisc.c
@@ -985,26 +985,31 @@ maintenance_print_one_line_table (struct symtab *symtab, void *data)
     printf_filtered (_("Line table has no lines.\n"));
   else
     {
-      int i;
-
       /* Leave space for 6 digits of index and line number.  After that the
 	 tables will just not format as well.  */
-      printf_filtered (_("%-6s %6s %s %s\n"),
-		       _("INDEX"), _("LINE"), _("ADDRESS"), _("IS-STMT"));
+      struct ui_out *uiout = current_uiout;
+      ui_out_emit_table table_emitter (uiout, 4, -1, "line-table");
+      uiout->table_header (6, ui_left, "index", _("INDEX"));
+      uiout->table_header (6, ui_left, "line", _("LINE"));
+      uiout->table_header (18, ui_left, "address", _("ADDRESS"));
+      uiout->table_header (1, ui_left, "is-stmt", _("IS-STMT"));
+      uiout->table_body ();
 
-      for (i = 0; i < linetable->nitems; ++i)
+      for (int i = 0; i < linetable->nitems; ++i)
 	{
 	  struct linetable_entry *item;
 
 	  item = &linetable->item [i];
-	  printf_filtered ("%-6d ", i);
+	  ui_out_emit_tuple tuple_emitter (uiout, nullptr);
+	  uiout->field_signed ("index", i);
 	  if (item->line > 0)
-	    printf_filtered ("%6d ", item->line);
+	    uiout->field_signed ("line", item->line);
 	  else
-	    printf_filtered ("%6s ", _("END"));
-	  printf_filtered ("%s%s\n",
-			   core_addr_to_string (item->pc),
-			   (item->is_stmt ? " Y" : ""));
+	    uiout->field_string ("line", _("END"));
+	  uiout->field_core_addr ("address", get_objfile_arch (objfile),
+				  item->pc);
+	  uiout->field_string ("is-stmt", item->is_stmt ? "Y" : "");
+	  uiout->text ("\n");
 	}
     }
 

andestech/gdb

Conflicts:
	gdb/ChangeLog
	gdb/testsuite/ChangeLog

gdb/symmisc.c
@@ -999,26 +999,31 @@ maintenance_print_one_line_table (struct symtab *symtab, void *data)
     printf_filtered (_("Line table has no lines.\n"));
   else
     {
-      int i;
-
       /* Leave space for 6 digits of index and line number.  After that the
 	 tables will just not format as well.  */
-      printf_filtered (_("%-6s %6s %s %s\n"),
-		       _("INDEX"), _("LINE"), _("ADDRESS"), _("IS-STMT"));
+      struct ui_out *uiout = current_uiout;
+      ui_out_emit_table table_emitter (uiout, 4, -1, "line-table");
+      uiout->table_header (6, ui_left, "index", _("INDEX"));
+      uiout->table_header (6, ui_left, "line", _("LINE"));
+      uiout->table_header (18, ui_left, "address", _("ADDRESS"));
+      uiout->table_header (1, ui_left, "is-stmt", _("IS-STMT"));
+      uiout->table_body ();
 
-      for (i = 0; i < linetable->nitems; ++i)
+      for (int i = 0; i < linetable->nitems; ++i)
 	{
 	  struct linetable_entry *item;
 
 	  item = &linetable->item [i];
-	  printf_filtered ("%-6d ", i);
+	  ui_out_emit_tuple tuple_emitter (uiout, nullptr);
+	  uiout->field_int ("index", i);
 	  if (item->line > 0)
-	    printf_filtered ("%6d ", item->line);
+	    uiout->field_int ("line", item->line);
 	  else
-	    printf_filtered ("%6s ", _("END"));
-	  printf_filtered ("%s%s\n",
-			   core_addr_to_string (item->pc),
-			   (item->is_stmt ? " Y" : ""));
+	    uiout->field_string ("line", _("END"));
+	  uiout->field_core_addr ("address", get_objfile_arch (objfile),
+				  item->pc);
+	  uiout->field_string ("is-stmt", item->is_stmt ? "Y" : "");
+	  uiout->text ("\n");
 	}
     }
 