3d97f08f139b85806d724c863430493e16540714
Penny Chen <penny@andestech.com>
2022-11-16 09:55:45 +0800

    gdb: unconditionally define _initialize_string_view_selftests
    The build failure was noticed by Helmut Jarausch in
    https://bugs.gentoo.org/680232:
        $ ./configure CXXFLAGS='-std=c++17 -Os'
    		  ...
    				      CXXLD  gdb
    			ld: init.o: in function `initialize_all_files()':
    			init.c:(.text+0x113): undefined reference to `_initialize_string_view_selftests()'
    
    It happens because '_initialize_string_view_selftests()' is
    conditionally defined based on C++ default.
    
    The change defines '_initialize_string_view_selftests()'
    unconditionally and leaves implementation a no-op on c++17
    compilers.

--- gdb/unittests/string_view-selftests.c
+++ gdb/unittests/string_view-selftests.c
@@ -170,10 +170,14 @@ run_tests ()
 } /* namespace string_view */
 } /* namespace selftests */
 
+#endif /* __cplusplus < 201703L */
+
+void _initialize_string_view_selftests ();
 void
 _initialize_string_view_selftests ()
 {
+#if defined(GDB_STRING_VIEW)
   selftests::register_test ("string_view", selftests::string_view::run_tests);
+#endif
 }
 
-#endif /* __cplusplus < 201703L */
