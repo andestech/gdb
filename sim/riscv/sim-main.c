f52e2f4631f23c7025758a2c1d7bdd6908a8f314
Jim Tsung-Chun Lin <jim@andestech.com>
2024-01-18 10:06:15 +0800

    (sim) Add missing inst class INSN_CLASS_F_AND_ZFHMIN_BF16 for zfh

--- sim/riscv/sim-main.c
+++ sim/riscv/sim-main.c
@@ -9309,6 +9309,7 @@ execute_one (SIM_CPU *cpu, unsigned_word iw, const struct riscv_opcode *op, int 
       return execute_andes(cpu, iw, op, ex9);
     case INSN_CLASS_F_AND_ZFH:
     case INSN_CLASS_F_AND_ZFHMIN:
+    case INSN_CLASS_F_AND_ZFHMIN_BF16:
     case INSN_CLASS_D_AND_ZFH:
     case INSN_CLASS_D_AND_ZFHMIN:
       return execute_zfh (cpu, iw, op, ex9);
